e1b5c91334da6d44793592f56356269c000bde44
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-02-22 22:14:39 -0300

    Add AssemblyInfo

---
+++ ricaun.HelixToolkit.Wpf/Proprieties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://helix-toolkit.org/wpf", "HelixToolkit.Wpf")]
+[assembly: XmlnsPrefix("http://helix-toolkit.org/wpf", "h")]
