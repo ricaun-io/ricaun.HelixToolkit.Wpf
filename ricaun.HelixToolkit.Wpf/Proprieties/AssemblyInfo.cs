835bfd4b77904087e6d65b23de64858d4f27388a
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-03-03 15:56:34 -0300

    - Add AllowMultiple - Remove Attribute

--- ricaun.HelixToolkit.Wpf/Proprieties/AssemblyInfo.cs
+++ ricaun.HelixToolkit.Wpf/Proprieties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Windows.Markup;
+﻿//using System;
+//using System.Windows.Markup;
 
-[assembly: XmlnsDefinition("http://helix-toolkit.org/wpf", "HelixToolkit.Wpf")]
-[assembly: XmlnsPrefix("http://helix-toolkit.org/wpf", "h")]
+//[assembly: XmlnsDefinition("http://helix-toolkit.org/wpf", "HelixToolkit.Wpf")]
+//[assembly: XmlnsPrefix("http://helix-toolkit.org/wpf", "h")]
